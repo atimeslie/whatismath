0f01a45175e9010f76e70d98049a36cd84fb8286
lufan <lufan787@gmail.com>
2016-11-10 11:04:13 +0800

    first add

---
+++ aweiji/demo1/demo1/demo1.cpp
@@ -0,0 +1,22 @@
+// demo1.cpp : 定义控制台应用程序的入口点。
+//
+//a为基表示整数问题
+#include <stdio.h>
+#include <stdafx.h>
+
+void main()
+{
+	//int i = 0;
+	//int j = 0;
+	int a[14] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+	for (int j = 0; j < 14; j++)
+	{
+		int sum = 0;
+		for (int i = 2; i < a[j]; i++)
+		{
+			sum = sum +
+
+		}
+	}
+	
+}
